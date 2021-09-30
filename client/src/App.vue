387527e514baf69ac4bd57e7c0004b0884f61c9e
mario12335 <duspara777@hotmail.com>
2021-09-30 22:18:10 +0200

    new file:   .env.example 	new file:   .gitignore 	new file:   client/.browserslistrc 	new file:   client/.eslintrc.js 	new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/babel.config.js 	new file:   client/config.js 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/public/favicon.ico 	new file:   client/public/img/icons/android-chrome-192x192.png 	new file:   client/public/img/icons/android-chrome-512x512.png 	new file:   client/public/img/icons/android-chrome-maskable-192x192.png 	new file:   client/public/img/icons/android-chrome-maskable-512x512.png 	new file:   client/public/img/icons/apple-touch-icon-120x120.png 	new file:   client/public/img/icons/apple-touch-icon-152x152.png 	new file:   client/public/img/icons/apple-touch-icon-180x180.png 	new file:   client/public/img/icons/apple-touch-icon-60x60.png 	new file:   client/public/img/icons/apple-touch-icon-76x76.png 	new file:   client/public/img/icons/apple-touch-icon.png 	new file:   client/public/img/icons/favicon-16x16.png 	new file:   client/public/img/icons/favicon-32x32.png 	new file:   client/public/img/icons/msapplication-icon-144x144.png 	new file:   client/public/img/icons/mstile-150x150.png 	new file:   client/public/img/icons/safari-pinned-tab.svg 	new file:   client/public/index.html 	new file:   client/public/robots.txt 	new file:   client/src/App.vue 	new file:   client/src/assets/logo.png 	new file:   client/src/main.js 	new file:   client/src/registerServiceWorker.js 	new file:   client/vue.conf.js 	new file:   package-lock.json 	new file:   package.json 	new file:   server.js

---
+++ client/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <form  @submit.prevent="post">
+       <input v-model="form.name" type="text" name="name" id="name" placeholder="name"> <br>
+       <input  v-model="form.email" type="email" name="email" id="email" placeholder="email"> <br>
+       <textarea  v-model="form.message" name="message" id="message" cols="30" rows="10"></textarea> <br>
+        <input type="submit" class="submit">
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  components: {
+   
+  },
+  data(){
+    
+    return{
+      form:{
+        name:undefined,
+        email:undefined,
+        message:undefined
+      },
+      
+    }
+  },
+
+  methods: {
+    async post()
+    {
+      console.log(this.form);
+      await axios.post('http://localhost:5000/send-email',this.form).then(console.log("Email send")).catch(error => {
+        console.log(error);
+      })
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
