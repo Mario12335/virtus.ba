e0ab3ece6736afeb1a8345777a9dc750c8328fcd
Josip <j.leko@globalsoft.ba>
2021-10-01 00:13:41 +0200

    Updated frontend design

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import smoothScroll from "./assets/js/smoothscroll";
+
+Vue.use(smoothScroll)
+
+
 
 new Vue({
   render: h => h(App),
